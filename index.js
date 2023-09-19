162340d6521856b3e1bfce7b264eaf637a8d54a6
henry9405 <144359211+henry9405@users.noreply.github.com>
2023-09-19 11:23:21 +0300

    assignment submission

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(x){
+  if (x <= 400) 
+    {return 'This one is on me!'
+  } else if (x> 2000 && x <= 2500){
+    return 'I will gladly take your thirty bucks.'
+  } else {
+    return 'No can do.'
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+const y = 'NYC';
+function ternaryCheckCity(y){
+  return (y ==='NYC')
+? 'Ok, sounds good.' : 'No go.';
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+const z = 'generous' ;
+function switchOnCharmFromTip(z){
+  switch (z){
+    case 'generous' :
+      return ('Thank you so much.');
+
+      case 'not as generous':
+        return ('Thank you.');
+
+      default:
+        return ('Bye.')
+  }
 }
